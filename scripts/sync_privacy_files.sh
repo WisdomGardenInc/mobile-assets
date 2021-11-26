51667582667ff43ae198ee0b4f20cc4c60796f5c
liupeixin <fengyi.mail@gmail.com>
2021-11-26 12:15:17 +0800

    update privacy policy

---
+++ scripts/sync_privacy_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+APP_REPO_RELATIVE_PATH="../mobile-2.0"
+
+
+cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/privacy-policy-zh-CN.md .
+cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/privacy-policy-zh-TW.md .
+cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/privacy-policy-en.md .
+cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/privacy-policy-info.json .
+cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/privacy-policy.html .
