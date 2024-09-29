90a760054c51f1fc63245676681cc4ad4f3016cd
liupeixin <fengyi.mail@gmail.com>
2024-09-29 13:44:07 +0800

    update package.json

--- scripts/sync_version_files.sh
+++ scripts/sync_version_files.sh
@@ -2,6 +2,6 @@
 
 ASSISTANT_REPO_RELATIVE_PATH="../mobile-assistant"
 
-cp ${APP_REPO_RELATIVE_PATH}/version_update_android.json .
-cp ${APP_REPO_RELATIVE_PATH}/version_update_ios.json .
-cp ${APP_REPO_RELATIVE_PATH}/version_aligner.json .
+cp ${ASSISTANT_REPO_RELATIVE_PATH}/version_update_android.json .
+cp ${ASSISTANT_REPO_RELATIVE_PATH}/version_update_ios.json .
+cp ${ASSISTANT_REPO_RELATIVE_PATH}/version_aligner.json .
