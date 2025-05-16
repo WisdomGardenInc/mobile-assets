578b5f0d72c975bfba9de2efbb63309d117c6b64
yaoxiangyi <yaoxiangyi@wisdomgarden.com>
2025-05-16 14:07:33 +0800

    modify script

--- scripts/sync_version_files.sh
+++ scripts/sync_version_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ASSISTANT_REPO_RELATIVE_PATH="../mobile-assistant"
+ASSISTANT_REPO_RELATIVE_PATH="${1:-../mobile-assistant}"
 
 cp ${ASSISTANT_REPO_RELATIVE_PATH}/version_update_android.json .
 cp ${ASSISTANT_REPO_RELATIVE_PATH}/version_update_ios.json .
