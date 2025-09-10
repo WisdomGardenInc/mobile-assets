0374dcfd446375b9d115ef7cc3c919bc0263f1f9
liupeixin <fengyi.mail@gmail.com>
2025-09-10 15:38:10 +0800

    unify asset version numbers

--- scripts/sync_privacy_files.sh
+++ scripts/sync_privacy_files.sh
@@ -3,6 +3,8 @@
 APP_REPO_RELATIVE_PATH="../mobile-2.5"
 
 
+VERSION=$(node -p -e "require('${APP_REPO_RELATIVE_PATH}/package.json').version")
+
 cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/marked.min-4.0.1.js .
 cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/policy-common.js .
 cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/policy-common.css .
@@ -25,3 +27,18 @@ cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/children-privacy-policy-zh-CN.md
 
 cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/summary-privacy-policy.html .
 cp ${APP_REPO_RELATIVE_PATH}/doc/privacy-policy/summary-privacy-policy-zh-CN.md .
+
+FILES_TO_REPLACE_VERSION=(
+    "children-privacy-policy.html"
+    "privacy-policy.html"
+    "summary-privacy-policy.html"
+    "user-agreement.html"
+)
+
+for file in "${FILES_TO_REPLACE_VERSION[@]}"; do
+    if [ -f "$file" ]; then
+        gsed -i "s/v={version}/v=$VERSION/g" "$file"
+    else
+        echo "File not existed - $file"
+    fi
+done
